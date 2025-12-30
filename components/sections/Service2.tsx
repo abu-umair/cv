bd2645e0050044699cd9667eec4341ee018fdbdc
abu-umair <mrdwnw93@gmail.com>
2025-12-31 03:05:02 +0700

    ~ change text service

--- components/sections/Service2.tsx
+++ components/sections/Service2.tsx
@@ -59,8 +59,8 @@ export default function Service2() {
 												<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
 													<path d="M12 13.3336L2.77735 19.482C2.54759 19.6352 2.23715 19.5731 2.08397 19.3434C2.02922 19.2612 2 19.1647 2 19.066V4.93457C2 4.65842 2.22386 4.43457 2.5 4.43457C2.59871 4.43457 2.69522 4.46379 2.77735 4.51854L12 10.6669V4.93457C12 4.65842 12.2239 4.43457 12.5 4.43457C12.5987 4.43457 12.6952 4.46379 12.7774 4.51854L23.376 11.5842C23.6057 11.7374 23.6678 12.0479 23.5146 12.2776C23.478 12.3325 23.4309 12.3797 23.376 12.4163L12.7774 19.482C12.5476 19.6352 12.2372 19.5731 12.084 19.3434C12.0292 19.2612 12 19.1647 12 19.066V13.3336ZM10.3944 12.0003L4 7.73734V16.2632L10.3944 12.0003ZM14 7.73734V16.2632L20.3944 12.0003L14 7.73734Z" fill="#1F1F24" />
 												</svg>
-												<h6 className="my-3 fw-medium">Performance Optimization</h6>
-												<p className="fs-7 text-300 fw-regular">Enhancing web performance using <span className="text-secondary-2">Golang + gRPC</span> for high-efficiency backend services and <span className="text-secondary-2">Laravel</span> for robust web solutions. Specializing in low-latency communication and scalable architecture to deliver superior speed.</p>
+												<h6 className="my-3 fw-medium">Scalable System Architecture</h6>
+												<p className="fs-7 text-300 fw-regular">Designing robust <span className="text-secondary-2">Monolithic or Microservices architectures</span> tailored to your project scale. I optimize web performance using <span className="text-secondary-2">Golang + gRPC</span> for high-efficiency services and <span className="text-secondary-2">Laravel</span> for stable, maintainable backend solutions.</p>
 											</div>
 										</div>
 										<div className="col-lg-4 col-md-6">
@@ -78,7 +78,7 @@ export default function Service2() {
 													<path d="M16.9337 8.96494C16.426 5.03562 13.0675 2 9 2C4.58172 2 1 5.58172 1 10C1 11.8924 1.65707 13.6313 2.7555 15.0011C3.56351 16.0087 4.00033 17.1252 4.00025 18.3061L4 22H13L13.001 19H15C16.1046 19 17 18.1046 17 17V14.071L18.9593 13.2317C19.3025 13.0847 19.3324 12.7367 19.1842 12.5037L16.9337 8.96494ZM3 10C3 6.68629 5.68629 4 9 4C12.0243 4 14.5665 6.25141 14.9501 9.22118L15.0072 9.66262L16.5497 12.0881L15 12.7519V17H11.0017L11.0007 20H6.00013L6.00025 18.3063C6.00036 16.6672 5.40965 15.114 4.31578 13.7499C3.46818 12.6929 3 11.3849 3 10ZM21.1535 18.1024L19.4893 16.9929C20.4436 15.5642 21 13.8471 21 12.0001C21 10.153 20.4436 8.4359 19.4893 7.00722L21.1535 5.89771C22.32 7.64386 23 9.74254 23 12.0001C23 14.2576 22.32 16.3562 21.1535 18.1024Z" fill="#1F1F24" />
 												</svg>
 												<h6 className="my-3 fw-medium">Integrating AI</h6>
-												<p className="fs-7 text-300 fw-regular">Boost your applications with AI for improved efficiency, automation, and enhanced user experience</p>
+												<p className="fs-7 text-300 fw-regular">Boost your <span className="text-secondary-2">applications with AI</span> for improved efficiency, automation, and enhanced user experience</p>
 											</div>
 										</div>
 									</div>
